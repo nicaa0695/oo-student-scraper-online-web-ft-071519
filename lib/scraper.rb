c548fc4a01195da610172271c4f47fa967d2421b
Veronica Lopez <nicaa0695@gmail.com>
2019-08-05 18:13:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
   students = []
   
   def self.scrape_index_page(profile_url)
-     doc = Nokogiri::HTML(open(index_url))
+     doc = Nokogiri::HTML(open())
        student_list = []
 
          doc.css('.student-card').each do |students|
