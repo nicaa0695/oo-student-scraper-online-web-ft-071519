3266b8e3e33f013272600410a65bd6138426bffa
Veronica Lopez <nicaa0695@gmail.com>
2019-08-05 17:56:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,4 +10,25 @@ class Scraper
      end 
    end 
    
+   def self.scrape_profile_page(profile_url)
+     student_doc = Nokogiri::HTML(open(profile_url))
+
+ 
+    end		     links = student_doc.css("div.social-icon-container a")
+     array_of_links = links.collect{|link| link['href']}
+
+      profile = {}
+     array_of_links.each do |social|
+       profile[:twitter] = social if social.include?("twitter")
+       profile[:linkedin] = social if social.include?("linkedin")
+       profile[:github] = social if social.include?("github")
+       profile[:blog] = social if !profile.has_value?(social) #!profile.include?(social)
+     end 
+
+      profile[:profile_quote] = student_doc.css("div.profile-quote").text
+     profile[:bio] = student_doc.css("div.description-holder p").text
+
+      profile 
+
+    end 
    
