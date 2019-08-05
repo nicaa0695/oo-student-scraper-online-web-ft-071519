c1ac1da7d5c7ecdfb8e250b034ad19e906ab7bd3
Veronica Lopez <nicaa0695@gmail.com>
2019-08-05 17:58:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,34 +1,3 @@
-class Scraper 
-  def self.scrape_index_page(index_url)
-  doc = Nokogiri::HTML(open(index_url))
-     students = doc.css("div.student-card").collect do |student|
-       {
-         :name => student.search("h4.student-name")[0].text,
-         :location => student.search("p.student-location")[0].text,
-         :profile_url => student.search("a")[0]["href"]
-       } 
-     end 
-   end 
-   
-   def self.scrape_profile_page(profile_url)
-     student_doc = Nokogiri::HTML(open(profile_url))
+require 'pry'
 
- 		 links = student_doc.css("div.social-icon-container a")
-     array_of_links = links.collect{|link| link['href']}
-
-      profile = {}
-     array_of_links.each do |social|
-       profile[:twitter] = social if social.include?("twitter")
-       profile[:linkedin] = social if social.include?("linkedin")
-       profile[:github] = social if social.include?("github")
-       profile[:blog] = social if !profile.has_value?(social) #!profile.include?(social)
-     end 
-
-
-      profile[:profile_quote] = student_doc.css("div.profile-quote").text
-     profile[:bio] = student_doc.css("div.description-holder p").text
-
-      profile 
-
-    end 
    
