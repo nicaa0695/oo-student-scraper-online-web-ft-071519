01a628b33d89907a5317e16990ac03358d471a70
Veronica Lopez <nicaa0695@gmail.com>
2019-08-05 17:54:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,2 +1,8 @@
 class Scraper 
-  
+  doc = Nokogiri::HTML(open(index_url))
+     students = doc.css("div.student-card").collect do |student|
+       {
+         :name => student.search("h4.student-name")[0].text,
+         :location => student.search("p.student-location")[0].text,
+         :profile_url => student.search("a")[0]["href"]
+       }
